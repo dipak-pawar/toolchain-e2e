3f8ba556cda5f7aae2df5fdebd4275860b8c645e
Dipak Pawar <dipakpawar231@gmail.com>
2019-11-14 13:26:18 +0530

    add make target to deploy required svc on host and member cluster

--- make/dev.mk
+++ make/dev.mk
@@ -29,3 +29,21 @@ dev-deploy-e2e-host-local:
 ## Deploy the e2e resources with the local 'registration-service' repository only
 dev-deploy-e2e-registration-local:
 	$(MAKE) dev-deploy-e2e REG_REPO_PATH=${PWD}/../registration-service
+
+.PHONY: dev-deploy-host-services
+dev-deploy-host-services:
+	$(MAKE) deploy-host-services HOST_NS=${DEV_HOST_NS} REGISTRATION_SERVICE_NS=${DEV_REGISTRATION_SERVICE_NS} HOST_IMAGE_NAME=quay.io/codeready-toolchain/host-operator:v0.1
+	@echo "Deployment complete on host cluster!"
+	@echo "To clean the cluster run 'make clean-e2e-resources'"
+
+.PHONY: dev-deploy-member-services
+dev-deploy-member-services:
+	$(MAKE) deploy-member-services MEMBER_NS=${DEV_MEMBER_NS} MEMBER_IMAGE_NAME=quay.io/codeready-toolchain/member-operator:v0.1
+	@echo "Deployment complete on member cluster!"
+	@echo "To clean the cluster run 'make clean-e2e-resources'"
+
+.PHONY: deploy-host-services
+deploy-host-services: build-with-operators login-as-admin deploy-host deploy-registration
+
+.PHONY: deploy-member-services
+deploy-member-services: build-with-operators login-as-admin deploy-member
